4fc72beec03cb29d25f577e63aa29df78cad96a7
Manish patil <manishpatil7058@gmail.com>
2026-01-24 20:33:06 +0530

    walk directory

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <lib.h> 
 #include <iostream>
-#include <tinydir.h>
 #include <tree_sitter/api.h>
 
 using namespace std;
@@ -9,29 +8,22 @@ extern "C" {
 const TSLanguage *tree_sitter_java(void);
 }
 
+ENTRY_ACTION printEntryInDir(WALK_ENTRY_STATUS walkResult, TargetEntry &entry) {
+  for(int i = 0; i< entry.level; i++){
+    cout << " ";
+  }
+  cout<<entry.name<<endl;
+  return ENTRY_ACTION::CONTINUE;
+}
+
 int main(int argc, char *argv[]) {
 
   cout << "Hello world" << endl;
-
-  /**
-   * 3. conditionals
-   * 2. threads
-   */
-  ReplaceEngine engine(tree_sitter_java());
-  File file = engine.readFile("sample.java");
-
-  file.print();
-  ReplaceASTAwareOpr rep("apache","xapachex", "import_declaration");
-  rep.apply(file);
-
-  ReplaceASTAwareOpr rep2("apache","xapachex", "local_variable_declaration");
-  rep2.apply(file);
+ 
+  TargetDir dir("./xyz");
+  cout << dir.name <<endl;
   
-  //SaveOpr save;
-  //save.apply(file);
+  cout << dir.walk(true, printEntryInDir) << endl;
 
-  // parseAndPrintFile("sample.java", tree_sitter_java());
-  //  printFileContent("sample.txt");
-  //  printDirectoryContent("./tree-sitter/lib", true);
   return 0;
 }
